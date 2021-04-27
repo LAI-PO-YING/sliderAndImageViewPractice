c0f33ae22dec65fc910cb7c23874467b0742ae99
Ivan Lai <229705jack2gmail.com>
2021-04-27 14:59:55 +0800

    first update

--- sliderAndImageViewPractice/ViewController.swift
+++ sliderAndImageViewPractice/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var rabbitImageView: UIImageView!
+    @IBOutlet weak var pImageView: UIImageView!
+    @IBOutlet weak var sliderValue: UISlider!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        flowerImageView.alpha = 0
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func sliderChange(_ sender: UISlider) {
+        pImageView.frame.origin.x = CGFloat(117 + sliderValue.value)
+        rabbitImageView.frame.origin.x = CGFloat(670 - sliderValue.value)
+        flowerImageView.alpha = CGFloat(sliderValue.value/223)
+        print(sliderValue.value)
+    }
 }
 
